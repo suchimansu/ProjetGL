f1909f65aee2b4f635cafffd1d1fe8604d40a7d1
Franck <Franck@172.16.106.22>
2015-12-03 15:23:09 +0100

    J'ai testé le commit avec un prout dans le main

--- ProjetGL/src/categorisation_image/Main.java
+++ ProjetGL/src/categorisation_image/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		        System.out.println("Yasmine Test");
-                        System.out.println("Franck Test");
+                        System.out.println("Franck Test2");
 
 	}
 
