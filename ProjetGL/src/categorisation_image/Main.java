410f8980b5cf72b7a78ae080be02088ac69474d0
Franck <Franck@172.16.106.22>
2015-12-03 15:39:21 +0100

    J'ai testé le commit avec un 8

--- ProjetGL/src/categorisation_image/Main.java
+++ ProjetGL/src/categorisation_image/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		        System.out.println("Yasmine Test");
-                        System.out.println("Franck Test7");
+                        System.out.println("Franck Test8");
                         	System.out.println("Damien Test3");
 
 	}
