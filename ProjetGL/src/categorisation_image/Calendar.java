b4f3b04a63238c9f1ed4ad7a99fb1a4f2b769013
suchi <suchi@S.home>
2015-12-08 00:27:07 +0100

    Avancé sur le calendrier. Premiers test, à ameliorer

--- ProjetGL/src/categorisation_image/Calendar.java
+++ ProjetGL/src/categorisation_image/Calendar.java
@@ -1,19 +1,62 @@
 package categorisation_image;
 
-import java.util.Date;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.TreeMap;
+
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Component;
+import net.fortuna.ical4j.model.Date;
+import net.fortuna.ical4j.model.DateTime;
+import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.PropertyList;
 
 public class Calendar {
 
 	// Type a valider
-	private TreeMap<Integer,Event> cal;
+	private TreeMap<Integer,Event> calMap;
+	private net.fortuna.ical4j.model.Calendar cal;
 	
 	public Calendar(){
 		
 	}
 	
 	public Calendar(String path){
+		try {
+			FileInputStream fis = new FileInputStream(path);
+			CalendarBuilder cbr = new CalendarBuilder();
+			cal = cbr.build(fis);
+			for (Iterator<?> i = cal.getComponents().iterator(); i.hasNext();) {
+			    Component component = (Component) i.next();
+			    System.out.println("Component [" + component.getName() + "]");
+			    DateTime start = new DateTime(component.getProperty(Property.DTSTART).getValue());
+			    System.out.println("start : " + start.getHours() + ":" + start.getMinutes());
+			    DateTime end = new DateTime(component.getProperty(Property.DTEND).getValue());
+			    System.out.println("end : " + end.getHours() + ":" + end.getMinutes());
+
+			    /*for (Iterator<?> j = component.getProperties().iterator(); j.hasNext();) {
+			        Property property = (Property) j.next();
+			        System.out.println("Property [" + property.getName() + ", " + property.getValue() + "]");
+			    }*/
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("Erreur lors de l'ouverture du fichier iCalendar dans le constructeur Calendar(String path) : fichier non trouvé.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Erreur lors de la lecture du fichier iCalendar dans le constructeur Calendar(String path).");
+			e.printStackTrace();
+		} catch (ParserException e) {
+			System.err.println("Erreur lors de la création de l'object net.fortuna.ical4j.model.Calendar dans le constructeur Calendar(String path).");
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
 		
 	}
 	
