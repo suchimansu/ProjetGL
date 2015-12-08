7881f6c2ce48180855b159d5e53d5e234c2969cd
Dysperia <softwatermermaid@hotmail.fr>
2015-12-08 16:51:56 +0100

    Ajout des classes de test vide (temporairement !)

---
+++ ProjetGL/src/test_categorisation_image/IntervalTest.java
@@ -0,0 +1,24 @@
+package test_categorisation_image;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IntervalTest {
+
+	@Test
+	public void testInterval() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEnd() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ ProjetGL/src/test_categorisation_image/ScanTest.java
@@ -0,0 +1,19 @@
+package test_categorisation_image;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScanTest {
+
+	@Test
+	public void testScan() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoScan() {
+		fail("Not yet implemented");
+	}
+
+}
